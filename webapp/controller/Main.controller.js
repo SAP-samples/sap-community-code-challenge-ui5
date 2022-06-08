67899200bf2fe547e0fb13196ae5176a402d11d0
marianfoo <13335743+marianfoo@users.noreply.github.com>
2022-06-08 10:17:46 +0200

    tests are green 🥳

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define([
+	"./BaseController"
+], function(BaseController) {
+	"use strict";
+
+	return BaseController.extend("ui5.challenge.controller.Main", {
+
+		onInit: function() {
+		},
+
+
+	});
+
+});
