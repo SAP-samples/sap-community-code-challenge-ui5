796a3318a131ebe82add631f950ef7439603bed6
DESKTOP-C3CDDM5\e_cas <e_castella@hotmail.com>
2022-06-09 23:49:06 +0200

    First week testing @enric101

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,23 @@
+sap.ui.define([
+	"./BaseController",
+    "sap/ui/model/json/JSONModel"
+], function(BaseController,JSONModel) {
+	"use strict";
+
+	return BaseController.extend("ui5.challenge.controller.Main", {
+
+		onInit: function() {
+            var data = [
+                {value:"A"},
+                {value:"B"},
+                {value:"C"}
+            ];
+
+            var oModel = new JSONModel(data);
+            this.getView().setModel(oModel,"list");
+
+		},
+
+	});
+
+}); 
