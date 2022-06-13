cc89d3c2889fcfca9a9b544182c2156a86a8a571
Bruenning, Richard <richard.bruenning@sew-eurodrive.de>
2022-06-13 09:46:32 +0200

    Tests successful

--- webapp/controller/BaseController.js
+++ webapp/controller/BaseController.js
@@ -1,17 +1,13 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function (Controller) {
-    "use strict";
+sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
+  "use strict";
 
-    return Controller.extend("ui5.challenge.controller.BaseController", {
-        navTo: function (psTarget, pmParameters, pbReplace) {
-            this.getRouter().navTo(psTarget, pmParameters, pbReplace);
-        },
-        getRouter: function () {
-            return UIComponent.getRouterFor(this);
-        },
-        onPress: function () {
-
-        }
-    });
+  return Controller.extend("ui5.challenge.controller.BaseController", {
+    navTo: function (psTarget, pmParameters, pbReplace) {
+      this.getRouter().navTo(psTarget, pmParameters, pbReplace);
+    },
+    getRouter: function () {
+      return UIComponent.getRouterFor(this);
+    },
+    onPress: function () {},
+  });
 });
