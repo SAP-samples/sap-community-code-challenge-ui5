045d196bdec25e2b5196dd70d52ca043bd5d1bc2
Julien Minder <julien.minder@flexso.com>
2022-06-14 12:21:48 +0200

    week 1 test passed

---
+++ webapp/controller/Main.Controller.js
@@ -0,0 +1,20 @@
+sap.ui.define(
+  [
+    "ui5/challenge/controller/BaseController",
+    "sap/m/Dialog",
+    "sap/m/Button",
+    "sap/m/Text",
+  ],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller, Dialog, Button, Text) {
+    "use strict";
+
+    return Controller.extend("ui5.challenge.controller.Main", {
+      /*
+     onTest: function () {},
+    */
+    });
+  }
+);
