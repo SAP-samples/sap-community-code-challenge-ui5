0ad33b5bf81905e756202654f4ed5f6a7b7ccc38
nair-sumesh <nair.sumesh@outlook.com>
2022-06-22 16:22:58 -0400

    Week 3

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,7 +1,11 @@
 sap.ui.define([
 	"./BaseController",
-    "sap/ui/model/json/JSONModel"
-], function(BaseController,JSONModel) {
+    "sap/ui/model/json/JSONModel",
+	"sap/m/Dialog",
+	"sap/m/List",
+	"sap/m/Button",
+	"sap/m/StandardListItem"
+], function(BaseController,JSONModel,Dialog,List,Button,StandardListItem) {
 	"use strict";
 
 	return BaseController.extend("ui5.challenge.controller.Detail", {
@@ -12,7 +16,38 @@ sap.ui.define([
         onNavigateToMain: function(oEvent){
             var oRouter = this.getOwnerComponent().getRouter();
 			oRouter.navTo("main");
-        }
+        },
+		onDetailDialogPress: function () {
+			if (!this.oResizableDialog) {
+				this.oResizableDialog = new Dialog({
+					id:"myDialog",
+					title: "Week 3 test",
+					contentWidth: "550px",
+					contentHeight: "300px",
+					resizable: true,
+					draggable: true,
+					content: new List({
+						items: {
+							path: "/todos",
+							template: new StandardListItem({
+								title: "{title}"
+							})
+						}
+					}),
+					endButton: new Button({
+						text: "Close",
+						press: function () {
+							this.oResizableDialog.close();
+						}.bind(this)
+					})
+				});
+
+				//to get access to the controller's model
+				this.getView().addDependent(this.oResizableDialog);
+			}
+
+			this.oResizableDialog.open();
+		},
     
 	});
 
