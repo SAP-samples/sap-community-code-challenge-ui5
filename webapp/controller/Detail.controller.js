f1cc80871afe353f1468f50de1a490cc204337eb
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-06-23 13:18:24 -0400

    Week 3

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,8 +1,16 @@
 sap.ui.define([
-    "./BaseController"
-], function (BaseController) {
+    "./BaseController",
+    "sap/m/Dialog"
+], function (BaseController, Dialog) {
     "use strict";
 
     return BaseController.extend("ui5.challenge.controller.Detail", {
+        onDialog: function () {
+            if (!this._dialog) {
+                this._dialog = new Dialog({ id: 'myDialog' })
+                this.getView().addDependent(this._dialog)
+            }
+            this._dialog.open()
+        }
     });
 });
