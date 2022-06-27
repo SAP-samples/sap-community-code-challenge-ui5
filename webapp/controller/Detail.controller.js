07fa6066aad8b55a64954784ac8807b47d9e1862
DESKTOP-C3CDDM5\e_cas <e_castella@hotmail.com>
2022-06-27 20:36:30 +0200

    week 3

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,7 +1,9 @@
 sap.ui.define([
 	"./BaseController",
-    "sap/ui/model/json/JSONModel"
-], function(BaseController,JSONModel) {
+    "sap/ui/model/json/JSONModel",
+	"sap/ui/core/routing/History",
+	"sap/m/Dialog"
+], function(BaseController,JSONModel,History,Dialog) {
 	"use strict";
 
 	return BaseController.extend("ui5.challenge.controller.Detail", {
@@ -9,6 +11,41 @@ sap.ui.define([
 		onInit: function() {
 
 		},
+
+		onNavBack: function () {
+			var oHistory = History.getInstance();
+			var sPreviousHash = oHistory.getPreviousHash();
+
+			if (sPreviousHash !== undefined) {
+				window.history.go(-1);
+			} else {
+				var oRouter = this.getOwnerComponent().getRouter();
+				oRouter.navTo("overview", {}, true);
+			}
+		},
+		/* Not get ID in test */
+		showDialog2 : function () {
+			// create dialog lazily
+			if (!this.pDialog) {
+				this.pDialog = this.loadFragment({
+					name: "ui5.challenge.view.HelloDialog"
+				});
+			}
+
+			this.pDialog.then(function(oDialog) {
+				oDialog.open();
+			});
+		},
+		showDialog : function () {
+            if (!this.dialog) {
+                this.dialog = new Dialog({
+                    id: "myDialog",
+                    title:"Hello",
+                })
+                this.getView().addDependent(this.dialog)
+            }
+            this.dialog.open()
+        }
 
 	});
 
